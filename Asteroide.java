7044d4bb201d8f665f5365c2955cc0f847c28516
Vinícius Lima <viniciuslucena303@gmail.com>
2025-09-21 23:33:27 -0300

    Update Asteroide.java

--- Asteroide.java
+++ Asteroide.java
@@ -1,28 +1,60 @@
+
 import java.util.Set;
 
 public class Asteroide extends CorpoCeleste {
 
     private String tipoOrbital;
     private String grupoOrbital;
-    private boolean ePerigoso;
+    private boolean perigoso;
 
     public Asteroide(String nome, String localizacao, boolean luzPropria, double massa, double diametro,
-                     Set<ComposicaoTipo> composicao, String orbita,
-                     String tipoOrbital, String grupoOrbital, boolean ePerigoso) {
+            Set<ComposicaoTipo> composicao, String orbita,
+            String tipoOrbital, String grupoOrbital, boolean perigoso) {
         super(nome, localizacao, luzPropria, massa, diametro, composicao, orbita);
         this.tipoOrbital = tipoOrbital;
         this.grupoOrbital = grupoOrbital;
-        this.ePerigoso = ePerigoso;
+        this.perigoso = perigoso;
+    }
+
+    public String getTipoOrbital() {
+        return tipoOrbital;
+    }
+
+    public void setTipoOrbital(String tipoOrbital) {
+        this.tipoOrbital = tipoOrbital;
+    }
+
+    public String getGrupoOrbital() {
+        return grupoOrbital;
+    }
+
+    public void setGrupoOrbital(String grupoOrbital) {
+        this.grupoOrbital = grupoOrbital;
+    }
+
+    public boolean isPerigoso() {
+        return perigoso;
+    }
+
+    public void setPerigoso(boolean perigoso) {
+        this.perigoso = perigoso;
     }
 
     public void verificarPerigo() {
-        // Exemplo simplificado: digamos que asteróides maiores que certa massa e em certo grupo são perigosos
-        if (getMassa() > 1e12 && grupoOrbital.equalsIgnoreCase("Apollo")) {
-            this.ePerigoso = true;
-            System.out.println("Alerta! Asteróide perigoso detectado!");
-        } else {
-            this.ePerigoso = false;
-            System.out.println("Asteróide não apresenta perigo para a Terra.");
+        boolean criterioTamanho = getDiametro() > 0.14;
+        boolean criterioGrupo = grupoOrbital.equalsIgnoreCase("Apollo")
+                || grupoOrbital.equalsIgnoreCase("Aten")
+                || grupoOrbital.equalsIgnoreCase("Atira")
+                || grupoOrbital.equalsIgnoreCase("Amor");
+        boolean criterioMassa = getMassa() > 1e10;
+
+        perigoso = criterioTamanho && criterioGrupo && criterioMassa;
+
+        if (perigoso) {
+            System.out.println("Asteroide perigoso para a Terra!");
+            System.out.println("   - Diâmetro: " + getDiametro() + " km");
+            System.out.println("   - Grupo orbital: " + grupoOrbital);
+            System.out.println("   - Massa: " + String.format("%.2e", getMassa()) + " kg");
         }
     }
 
@@ -31,6 +63,8 @@ public class Asteroide extends CorpoCeleste {
         super.imprimirInfos();
         System.out.println("Tipo Orbital: " + tipoOrbital);
         System.out.println("Grupo Orbital: " + grupoOrbital);
-        System.out.println("É perigoso? " + (ePerigoso ? "Sim" : "Não"));
+        verificarPerigo();
+        System.out.println("É perigoso? " + (perigoso ? "Sim" : "Não"));
+        System.out.println("------------------------------------\n");
     }
 }
