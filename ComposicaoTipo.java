6789b582069a597e805e0ea703d2f92c8659ec5a
Vinícius Lima <viniciuslucena303@gmail.com>
2025-09-21 23:33:50 -0300

    Update ComposicaoTipo.java

--- ComposicaoTipo.java
+++ ComposicaoTipo.java
@@ -1,6 +1,27 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collections;
+
 public enum ComposicaoTipo {
     ROCHAS,
     METAIS,
     GELO,
-    GASES
+    GASES;
+
+    public static Set<ComposicaoTipo> getTodos() {
+        Set<ComposicaoTipo> tudoJunto = new HashSet<>();
+        tudoJunto.add(ROCHAS);
+        tudoJunto.add(METAIS);
+        tudoJunto.add(GELO);
+        tudoJunto.add(GASES);
+        return Collections.unmodifiableSet(tudoJunto);
+    }
+
+    public static boolean isTodos(Set<ComposicaoTipo> composicao) {
+        return composicao != null && composicao.size() == 4 && 
+               composicao.contains(ROCHAS) && 
+               composicao.contains(METAIS) && 
+               composicao.contains(GELO) && 
+               composicao.contains(GASES);
+    }
 }
